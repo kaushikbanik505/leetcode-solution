2bfec3e4aba8ca65b8d4a4bcc4c2ba9f21995d2a
Spidy <166904630+kaushikbanik505@users.noreply.github.com>
2026-09-24 21:15:33 +0530

    Time: 0 ms (100%), Space: 44.3 MB (35.32%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] runningSum(int[] nums) 
+    {
+        for(int i=0 ; i<nums.length;i++)
+        {
+            if(i-1>=0)
+            {
+                nums[i]=nums[i]+nums[i-1];
+            }
+
+            else 
+            {
+                nums[i]=nums[i];
+            }
+        }
+
+        return nums;
+    }
+}
