b04acd129cf15ee3dd127649e86f76c78641cbcd
Spidy <166904630+kaushikbanik505@users.noreply.github.com>
2026-09-25 20:22:17 +0530

    Time: 3 ms (34.32%), Space: 63 MB (48.6%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            maxArea = max(maxArea, h * w);
+
+            // Move the pointer with the smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
